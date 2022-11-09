b3c416c0bd6344e688f35736b8c2b6921ceb5577
Stuzja <ann.shishkina04@mail.ru>
2022-11-09 16:24:33 +0300

    Feat: добавлены основные иконки и картинки

---
+++ lib/utils/app_icons.dart
@@ -0,0 +1,60 @@
+abstract class AppIcons {
+  static const String autoGraphDisabled =
+      "packages/core_ui/assets/icons/auto_graph_disabled.svg";
+  static const String autoGraphEnabled =
+      "packages/core_ui/assets/icons/auto_graph_enabled.svg";
+  static const String autoRenew = "packages/core_ui/assets/icons/autorenew.svg";
+  static const String bedtimeDisabled =
+      "packages/core_ui/assets/icons/bedtime_disabled.svg";
+  static const String bedtimeEnabled =
+      "packages/core_ui/assets/icons/bedtime_enabled.svg";
+  static const String favorite = "packages/core_ui/assets/icons/favorite.svg";
+  static const String handyman = "packages/core_ui/assets/icons/handyman.svg";
+  static const String localMallDisabled =
+      "packages/core_ui/assets/icons/local_mall_disabled.svg";
+  static const String localMallEnabled =
+      "packages/core_ui/assets/icons/local_mall_enabled.svg";
+  static const String precisionManufacturing =
+      "packages/core_ui/assets/icons/precision_manufacturing.svg";
+  static const String sell = "packages/core_ui/assets/icons/sell.svg";
+  static const String upgrade = "packages/core_ui/assets/icons/upgrade.svg";
+  static const String coin = "packages/core_ui/assets/icons/coin_icon.svg";
+  static const String code = "packages/core_ui/assets/icons/code_icon.svg";
+  static const String closed = 'packages/core_ui/assets/icons/close.svg';
+  static const String arrowRight =
+      'packages/core_ui/assets/icons/arrow_forward.svg';
+  static const String alarm = 'packages/core_ui/assets/icons/alarm.svg';
+  static const String qr = 'packages/core_ui/assets/icons/qr_code_scanner.svg';
+  static const String bedtime = 'packages/core_ui/assets/icons/bedtime.svg';
+  static const String wakeUp = 'packages/core_ui/assets/icons/wake_up.svg';
+  static const String coinCircle =
+      'packages/core_ui/assets/icons/coin_circle.svg';
+  static const String chair = 'packages/core_ui/assets/icons/chair.svg';
+  static const String gear = 'packages/core_ui/assets/icons/gear.svg';
+  static const String lockC = 'packages/core_ui/assets/icons/lock_C.svg';
+  static const String lockE = 'packages/core_ui/assets/icons/lock_E.svg';
+  static const String lockL = 'packages/core_ui/assets/icons/lock_L.svg';
+  static const String diamond = 'packages/core_ui/assets/icons/diamond.svg';
+  static const String speedometer =
+      'packages/core_ui/assets/icons/speedometer.svg';
+  static const String stars = 'packages/core_ui/assets/icons/stars.svg';
+  static const String sharp = 'packages/core_ui/assets/icons/sharp.svg';
+  static const String arrowRightAlt =
+      'packages/core_ui/assets/icons/arrow_right.svg';
+  static const String editCircle = 'packages/core_ui/assets/icons/editCircle.svg';
+  static const String edit = 'packages/core_ui/assets/icons/edit.svg';
+  static const String asleepAfter = "packages/core_ui/assets/icons/asleep_after.svg";
+  static const String noise = "packages/core_ui/assets/icons/noise.svg";
+  static const String wentToBed = "packages/core_ui/assets/icons/went_to_bed.svg";
+  static const String wokeUp = "packages/core_ui/assets/icons/woke_up.svg";
+  static const String box = "packages/core_ui/assets/icons/box.svg";
+  static const String moon = "packages/core_ui/assets/icons/moon.svg";
+  static const String female = 'packages/core_ui/assets/icons/female.svg';
+  static const String male = 'packages/core_ui/assets/icons/male.svg';
+  static const String transgender = 'packages/core_ui/assets/icons/transgender.svg';
+  static const String question = 'packages/core_ui/assets/icons/question_mark.svg';
+  static const String settings = 'packages/core_ui/assets/icons/settings.svg';
+  static const String volume = 'packages/core_ui/assets/icons/volume.svg';
+  static const String bnb = "packages/core_ui/assets/icons/bnb.svg";
+  static const String slt = "packages/core_ui/assets/icons/coin_slt.svg";
+}
