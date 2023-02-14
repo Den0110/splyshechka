d5f4e3a25d5ef5767cfcacdf700ac38f3bfa525d
Вова Афонченков <afonchenkov98@bk.ru>
2023-02-14 16:17:39 +0300

    feat: Добавлена функция отслеживания уровня громкости

--- lib/pages/alarm/sleeping/alarm_sleeping_page.dart
+++ lib/pages/alarm/sleeping/alarm_sleeping_page.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:noise_meter/noise_meter.dart';
 import 'package:splyshechka/utils/app_colors.dart';
 import 'package:splyshechka/utils/app_icons.dart';
 import 'package:splyshechka/utils/app_text_styles.dart';
@@ -15,9 +16,67 @@ import 'package:splyshechka/navigation/auto_router.gr.dart';
 import 'package:splyshechka/pages/alarm/sleeping/bloc/alarm_sleeping_cubit.dart';
 import 'package:splyshechka/pages/alarm/sleeping/widgets/sound_listener.dart';
 import 'package:splyshechka/utils/one_shot_bloc.dart';
+import 'dart:async';
 
-class AlarmSleepingPage extends StatelessWidget {
+class AlarmSleepingPage extends StatefulWidget {
   const AlarmSleepingPage({super.key});
+
+  @override
+  State<AlarmSleepingPage> createState() => _AlarmSleepingPageState();
+}
+
+class _AlarmSleepingPageState extends State<AlarmSleepingPage> {
+  bool _isRecording = false;
+  StreamSubscription<NoiseReading>? _noiseSubscription;
+  late NoiseMeter _noiseMeter;
+  int db = 0;
+  List<int> dbArr = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _noiseMeter = NoiseMeter(onError);
+  }
+
+  @override
+  void dispose() {
+    _noiseSubscription?.cancel();
+    super.dispose();
+  }
+
+  int counter = 0;
+  void onData(NoiseReading noiseReading) {
+    setState(() {
+      if (!_isRecording) {
+        _isRecording = true;
+      }
+    });
+    db = noiseReading.meanDecibel.round();
+    counter += 1;
+    if (counter % 100 == 0) dbArr.add(db);
+  }
+
+  void onError(Object error) {
+    _isRecording = false;
+  }
+
+  void start() async {
+    try {
+      _noiseSubscription = _noiseMeter.noiseStream.listen(onData);
+    } catch (err) {}
+  }
+
+  void stop() async {
+    try {
+      if (_noiseSubscription != null) {
+        _noiseSubscription!.cancel();
+        _noiseSubscription = null;
+      }
+      setState(() {
+        _isRecording = false;
+      });
+    } catch (err) {}
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +93,7 @@ class AlarmSleepingPage extends StatelessWidget {
         builder: (context, state) {
           return state.maybeWhen(
             initial: (currentDate, alarmTime, currentVolume) {
+              currentVolume = db;
               String dateString = DateFormat("EEEE, MMM d").format(currentDate);
               String timeString =
                   DateFormat(DateFormat.HOUR24_MINUTE).format(currentDate);
@@ -84,7 +144,7 @@ class AlarmSleepingPage extends StatelessWidget {
                       height: 115.h,
                     ),
                     SoundListener(
-                      waveHeight: currentVolume.toDouble(),
+                      waveHeight: currentVolume.toDouble() / 7,
                     ),
                     SizedBox(
                       height: 120.h,
@@ -98,6 +158,11 @@ class AlarmSleepingPage extends StatelessWidget {
                     SizedBox(
                       height: 45.h,
                     ),
+                    LargeButton(
+                        text: _isRecording
+                            ? 'Громкость в дб: $db'
+                            : 'Начать запись',
+                        onPressed: _isRecording? stop: start),
                     Padding(
                       padding: EdgeInsets.only(left: 25.w, right: 25.w),
                       child: LargeButton(
@@ -107,6 +172,7 @@ class AlarmSleepingPage extends StatelessWidget {
                         shadowColor: AppColors.black,
                         onPressed: () {
                           context.read<AlarmSleepingCubit>().wakeUpClicked();
+                          stop;
                         },
                       ),
                     ),
