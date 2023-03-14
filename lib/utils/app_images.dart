d0e8d8be137845689f4c17afa9a565369c7c0576
Вова Афонченков <afonchenkov98@bk.ru>
2023-03-14 18:33:45 +0300

    Добавлена страница ачивок

--- lib/utils/app_images.dart
+++ lib/utils/app_images.dart
@@ -1,6 +1,20 @@
 abstract class AppImages {
- static const String sleepingFace =
+  static const String sleepingFace =
       "https://drive.google.com/uc?export=view&id=1gnHBRgL1gV0Q7ewRt8bywXeVVF4M_Fmu";
   static const String clownFace =
       "https://drive.google.com/uc?export=view&id=1lXj3z1-fONnscN-lPxaGenInu8r0qe7U";
+
+  static const String woodSword = "assets/images/wood_sword.gif";
+  static const String ironSword = "assets/images/iron_sword.gif";
+  static const String goldenSword = "assets/images/golden_sword.gif";
+  static const String diamonSword = "assets/images/diamond_sword.gif";
+  static const String book = "assets/images/book.gif";
+  static const String crystal = "assets/images/crystal.gif";
+  static const String portal = "assets/images/portal.gif";
+  static const String gast = "assets/images/gast.gif";
+  static const String redstone = "assets/images/redstone.png";
+  static const String bed = "assets/images/bed.png";
+  static const String endPortal = "assets/images/endportal.gif";
+  static const String spiderEye = "assets/images/spider_eye.png";
+  static const String apple = "assets/images/apple.gif";
 }
