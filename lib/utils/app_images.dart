ce7b8667a53ff75783a2f1f1a260c2eef3ca22c2
kotjl <kotjleta161rus@mail.ru>
2023-03-29 20:10:37 +0300

    feat: Добавлены аватарки

--- lib/utils/app_images.dart
+++ lib/utils/app_images.dart
@@ -1,6 +1,25 @@
 abstract class AppImages {
- static const String sleepingFace =
+  static const String sleepingFace =
       "https://drive.google.com/uc?export=view&id=1gnHBRgL1gV0Q7ewRt8bywXeVVF4M_Fmu";
   static const String clownFace =
       "https://drive.google.com/uc?export=view&id=1lXj3z1-fONnscN-lPxaGenInu8r0qe7U";
+  static const String starFace =
+      "https://i.ibb.co/qd0bzqF/removal-ai-tmp-64242f69e20a6.png";
+  static const String moneyFace =
+      "https://i.ibb.co/V32XMHL/removal-ai-tmp-64242fd5d9c8a.png";
+  static const String shushingFace =
+      "https://i.ibb.co/c6M0DH1/removal-ai-tmp-64242fca7546d.png";
+  static const String smileyFace =
+      "https://i.ibb.co/xFvGp0p/removal-ai-tmp-64243060965d3.png";
+  static const String surprisedFace =
+      "https://i.ibb.co/kcNB9Wm/removal-ai-tmp-6424303ebf816.png";
+  static const String vomitFace =
+        "https://i.ibb.co/RNFsvHz/removal-ai-tmp-64243034a2964.png  ";
+  static const String embrassedFace =
+      "https://i.ibb.co/xmtFKHC/removal-ai-tmp-6424302b089d6.png";
+  static const String deadFace =
+      "https://i.ibb.co/vcvrxX0/removal-ai-tmp-6424301fa1283.png";
+
+      
+  static const String pillow = "assets/images/pillow.png";
 }
